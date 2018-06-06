738ec1cefe6c38d567848439f74b08e969a1defb
Phillipus <p.beauvoir@dadabeatnik.com>
2018-06-06 10:37:00 +0100

    Add more tests

--- com.archimatetool.script.tests/src/com/archimatetool/script/dom/model/EObjectProxyCollectionTests.java
+++ com.archimatetool.script.tests/src/com/archimatetool/script/dom/model/EObjectProxyCollectionTests.java
@@ -5,6 +5,13 @@
  */
 package com.archimatetool.script.dom.model;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+import com.archimatetool.model.IArchimateFactory;
+
 import junit.framework.JUnit4TestAdapter;
 
 
@@ -18,5 +25,20 @@ public class EObjectProxyCollectionTests {
     public static junit.framework.Test suite() {
         return new JUnit4TestAdapter(EObjectProxyCollectionTests.class);
     }
+    
+    @Test
+    public void first() {
+        // Empty
+        EObjectProxyCollection collection = new EObjectProxyCollection();
+        assertNull(collection.first());
+        
+        // First
+        EObjectProxy proxy1 = EObjectProxy.get(IArchimateFactory.eINSTANCE.createBusinessRole());
+        EObjectProxy proxy2 = EObjectProxy.get(IArchimateFactory.eINSTANCE.createBusinessRole());
+        collection.add(proxy1);
+        collection.add(proxy2);
+        assertEquals(proxy1, collection.first());
+    }
+    
     
 }
--- com.archimatetool.script.tests/src/com/archimatetool/script/dom/model/SelectorFilterFactoryTests.java
+++ com.archimatetool.script.tests/src/com/archimatetool/script/dom/model/SelectorFilterFactoryTests.java
@@ -5,6 +5,17 @@
  */
 package com.archimatetool.script.dom.model;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.archimatetool.model.IArchimateConcept;
+import com.archimatetool.model.IArchimateFactory;
+import com.archimatetool.model.IDiagramModel;
+import com.archimatetool.model.IFolder;
+import com.archimatetool.script.dom.model.SelectorFilterFactory.ISelectorFilter;
+
 import junit.framework.JUnit4TestAdapter;
 
 
@@ -13,6 +24,7 @@ import junit.framework.JUnit4TestAdapter;
  * 
  * @author Phillip Beauvoir
  */
+@SuppressWarnings("nls")
 public class SelectorFilterFactoryTests {
     
     public static junit.framework.Test suite() {
@@ -20,5 +32,155 @@ public class SelectorFilterFactoryTests {
     }
     
     
+    @Test
+    public void accept_All() {
+        ISelectorFilter filter = SelectorFilterFactory.INSTANCE.getFilter("*");
+        
+        assertTrue(filter.accept(IArchimateFactory.eINSTANCE.createBusinessRole()));
+        assertTrue(filter.accept(IArchimateFactory.eINSTANCE.createAssociationRelationship()));
+        assertTrue(filter.accept(IArchimateFactory.eINSTANCE.createSketchModel()));
+        assertTrue(filter.accept(IArchimateFactory.eINSTANCE.createArchimateDiagramModel()));
+        assertTrue(filter.accept(IArchimateFactory.eINSTANCE.createFolder()));
+        assertTrue(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelGroup()));
+        assertTrue(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelNote()));
+        assertTrue(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelConnection()));
+        assertTrue(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelReference()));
+        assertTrue(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelArchimateObject()));
+        assertTrue(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelArchimateConnection()));
+        
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createBounds()));
+    }
+    
+    @Test
+    public void accept_Concepts() {
+        ISelectorFilter filter = SelectorFilterFactory.INSTANCE.getFilter("concepts");
+        
+        assertTrue(filter.accept(IArchimateFactory.eINSTANCE.createBusinessRole()));
+        assertTrue(filter.accept(IArchimateFactory.eINSTANCE.createAssociationRelationship()));
+        
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createSketchModel()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createArchimateDiagramModel()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createFolder()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelGroup()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelNote()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelConnection()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelReference()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelArchimateObject()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelArchimateConnection()));
+    }
+    
+    @Test
+    public void accept_Elements() {
+        ISelectorFilter filter = SelectorFilterFactory.INSTANCE.getFilter("elements");
+        
+        assertTrue(filter.accept(IArchimateFactory.eINSTANCE.createBusinessRole()));
+
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createAssociationRelationship()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createSketchModel()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createArchimateDiagramModel()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createFolder()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelGroup()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelNote()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelConnection()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelReference()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelArchimateObject()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelArchimateConnection()));
+    }
+    
+    @Test
+    public void accept_Relations() {
+        ISelectorFilter filter = SelectorFilterFactory.INSTANCE.getFilter("relations");
+        
+        assertTrue(filter.accept(IArchimateFactory.eINSTANCE.createAssociationRelationship()));
+
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createBusinessRole()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createSketchModel()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createArchimateDiagramModel()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createFolder()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelGroup()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelNote()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelConnection()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelReference()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelArchimateObject()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelArchimateConnection()));
+    }
+
+    @Test
+    public void accept_Views() {
+        ISelectorFilter filter = SelectorFilterFactory.INSTANCE.getFilter("views");
+        
+        assertTrue(filter.accept(IArchimateFactory.eINSTANCE.createSketchModel()));
+        assertTrue(filter.accept(IArchimateFactory.eINSTANCE.createArchimateDiagramModel()));
+
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createAssociationRelationship()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createBusinessRole()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createFolder()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelGroup()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelNote()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelConnection()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelReference()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelArchimateObject()));
+        assertFalse(filter.accept(IArchimateFactory.eINSTANCE.createDiagramModelArchimateConnection()));
+    }
+    
+    @Test
+    public void accept_ID() {
+        IArchimateConcept concept = IArchimateFactory.eINSTANCE.createBusinessRole();
+        concept.setId("123");
+        
+        ISelectorFilter filter = SelectorFilterFactory.INSTANCE.getFilter("#123");
+        assertTrue(filter.accept(concept));
+        
+        filter = SelectorFilterFactory.INSTANCE.getFilter("#1234");
+        assertFalse(filter.accept(concept));
+    }
+    
+    @Test
+    public void accept_Name() {
+        IArchimateConcept concept = IArchimateFactory.eINSTANCE.createBusinessRole();
+        concept.setName("foo");
+        
+        ISelectorFilter filter = SelectorFilterFactory.INSTANCE.getFilter(".foo");
+        assertTrue(filter.accept(concept));
+        
+        filter = SelectorFilterFactory.INSTANCE.getFilter(".foot");
+        assertFalse(filter.accept(concept));
+    }
+    
+    @Test
+    public void accept_TypeName() {
+        IArchimateConcept concept = IArchimateFactory.eINSTANCE.createBusinessRole();
+        concept.setName("foo");
+        
+        ISelectorFilter filter = SelectorFilterFactory.INSTANCE.getFilter("BusinessRole.foo");
+        assertTrue(filter.accept(concept));
+        
+        filter = SelectorFilterFactory.INSTANCE.getFilter("BusinessActor.foo");
+        assertFalse(filter.accept(concept));
+    }
+    
+    @Test
+    public void accept_Type() {
+        IArchimateConcept element = IArchimateFactory.eINSTANCE.createBusinessRole();
+        IArchimateConcept relation = IArchimateFactory.eINSTANCE.createAssociationRelationship();
+        IDiagramModel dm = IArchimateFactory.eINSTANCE.createArchimateDiagramModel();
+        IFolder folder = IArchimateFactory.eINSTANCE.createFolder();
+        
+        ISelectorFilter filter = SelectorFilterFactory.INSTANCE.getFilter("BusinessRole");
+        assertTrue(filter.accept(element));
+        filter = SelectorFilterFactory.INSTANCE.getFilter("BusinessActor");
+        assertFalse(filter.accept(element));
+        
+        filter = SelectorFilterFactory.INSTANCE.getFilter("AssociationRelationship");
+        assertTrue(filter.accept(relation));
+        filter = SelectorFilterFactory.INSTANCE.getFilter("InfluenceRelationship");
+        assertFalse(filter.accept(relation));
+    
+        filter = SelectorFilterFactory.INSTANCE.getFilter("ArchimateDiagramModel");
+        assertTrue(filter.accept(dm));
+        
+        filter = SelectorFilterFactory.INSTANCE.getFilter("Folder");
+        assertTrue(filter.accept(folder));
+    }
 
 }
