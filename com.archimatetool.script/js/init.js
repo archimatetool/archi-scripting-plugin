4462f617301a78c99160c1de582892e52321cf73
Phillipus <p.beauvoir@dadabeatnik.com>
2018-06-28 11:56:38 +0100

    Don't use $(model) construct

--- com.archimatetool.script/js/init.js
+++ com.archimatetool.script/js/init.js
@@ -1,5 +1,5 @@
 function jArchi (obj) {
-	if (obj == model || obj == selection) // obj is an object
+	if (obj == selection) // obj is an object
 		return obj;
 	else // obj is an object, a string or will be casted to a (potentially empty) string
 		return model.find(obj || "");
