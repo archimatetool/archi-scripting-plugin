dc3c07c24a9170d2d7aafa2bf332c6f1eda8c92d
Phillipus <p.beauvoir@dadabeatnik.com>
2018-07-08 17:57:47 +0100

    Add model#renderViewAsBase64()

--- com.archimatetool.script/src/com/archimatetool/script/dom/model/Model.java
+++ com.archimatetool.script/src/com/archimatetool/script/dom/model/Model.java
@@ -5,28 +5,39 @@
  */
 package com.archimatetool.script.dom.model;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Base64.Encoder;
 
 import org.eclipse.osgi.util.NLS;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.ImageLoader;
 import org.eclipse.ui.PlatformUI;
 
+import com.archimatetool.editor.diagram.util.DiagramUtils;
 import com.archimatetool.editor.model.IArchiveManager;
 import com.archimatetool.editor.model.IEditorModelManager;
+import com.archimatetool.editor.ui.ImageFactory;
 import com.archimatetool.model.IArchimateFactory;
 import com.archimatetool.model.IArchimateModel;
 import com.archimatetool.script.ArchiScriptException;
 
 /**
- * Model load and create object
- * 
- * Represents the current model
- * This can be the selected model in focus if run in the UI, or the current model if run from the ACLI
- * It can be loaded from file, or created anew.
+ * Model utility functions
  * 
  * @author Phillip Beauvoir
  */
 public class Model {
     
+    /**
+     * Create a new model
+     * @param modelName the name
+     * @return the model
+     */
     public ArchimateModelProxy create(String modelName) {
         IArchimateModel model = IArchimateFactory.eINSTANCE.createArchimateModel();
         model.setDefaults();
@@ -40,6 +51,11 @@ public class Model {
         return new ArchimateModelProxy(model);
     }
     
+    /**
+     * Load and open a model
+     * @param path
+     * @return the model
+     */
     public ArchimateModelProxy load(String path) {
         File file = new File(path);
         
@@ -68,4 +84,64 @@ public class Model {
         throw new ArchiScriptException(NLS.bind(Messages.ArchimateModelProxy_2, path));
     }
     
+    /**
+     * Render a View as a String of BASE64 bytes
+     * @param dmProxy The DiagramModelProxy
+     * @param format One of "PNG", "BMP", "JPG", "JPEG",
+     * @param options can be scale and margin insets
+     * @return a string encoded in BASE64
+     * @throws IOException
+     */
+    public String renderViewAsBase64(DiagramModelProxy dmProxy, String format, int... options) throws IOException {
+        if(dmProxy == null || format == null) {
+            throw new ArchiScriptException("Null argument"); //$NON-NLS-1$
+        }
+        
+        // Default options
+        int scale = options.length > 0 ? options[0] : 1;
+        int margin = options.length > 1 ? options[1] : 10;
+        
+        // Format
+        int imgFormat = SWT.IMAGE_PNG;
+        
+        switch(format.toUpperCase()) {
+            case "PNG": //$NON-NLS-1$
+                imgFormat = SWT.IMAGE_PNG;
+                break;
+
+            case "BMP": //$NON-NLS-1$
+                imgFormat = SWT.IMAGE_BMP;
+                break;
+
+            case "GIF": //$NON-NLS-1$
+                imgFormat = SWT.IMAGE_GIF;
+                break;
+
+            case "JPG": //$NON-NLS-1$
+            case "JPEG": //$NON-NLS-1$
+                imgFormat = SWT.IMAGE_JPEG;
+                break;
+
+            default:
+                break;
+        }
+        
+        Image image = DiagramUtils.createImage(dmProxy.getEObject(), scale, margin);
+        
+        try {
+            ImageLoader loader = new ImageLoader();
+            loader.data = new ImageData[] { image.getImageData(ImageFactory.getDeviceZoom()) };
+            
+            try(ByteArrayOutputStream stream = new ByteArrayOutputStream(1024)) {
+                loader.save(stream, imgFormat);
+                
+                Encoder encoder = Base64.getEncoder();
+                return encoder.encodeToString(stream.toByteArray());
+            }
+        }
+        finally {
+            image.dispose();
+        }
+    }
+
 }
