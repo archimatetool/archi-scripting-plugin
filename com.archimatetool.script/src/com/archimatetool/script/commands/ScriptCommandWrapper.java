fde89ddc624ea0a8b14feee50e7ab5238c1a67b8
Phillipus <p.beauvoir@dadabeatnik.com>
2020-02-26 14:03:53 +0000

    Add ScriptCommandWrapper class
    * Can wrap non-jArchi Commands

---
+++ com.archimatetool.script/src/com/archimatetool/script/commands/ScriptCommandWrapper.java
@@ -0,0 +1,56 @@
+/**
+ * This program and the accompanying materials
+ * are made available under the terms of the License
+ * which accompanies this distribution in the file LICENSE.txt
+ */
+package com.archimatetool.script.commands;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gef.commands.Command;
+
+/**
+ * Script Command Wrapper around another non-jArchi Command
+ * 
+ * @author Phillip Beauvoir
+ */
+public class ScriptCommandWrapper extends ScriptCommand {
+    private Command cmd;
+
+    public ScriptCommandWrapper(Command cmd, EObject eObject) {
+        super(cmd.getLabel(), eObject);
+        this.cmd = cmd;
+    }
+    
+    @Override
+    public void perform() {
+        cmd.execute();
+    }
+    
+    @Override
+    public boolean canExecute() {
+        return cmd.canExecute();
+    }
+    
+    @Override
+    public boolean canRedo() {
+        return cmd.canRedo();
+    }
+    
+    @Override
+    public boolean canUndo() {
+        return cmd.canUndo();
+    }
+    
+    @Override
+    public void undo() {
+        cmd.undo();
+    }
+    
+    @Override
+    public void dispose() {
+        super.dispose();
+        cmd.dispose();
+        cmd = null;
+    }
+
+}
