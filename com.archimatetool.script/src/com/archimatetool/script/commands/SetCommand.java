1bee975c207be9de52588f3b4784e7cac895ebef
Phillipus <p.beauvoir@dadabeatnik.com>
2020-02-26 15:16:27 +0000

    SetCommand extends ScriptCommandWrapper

--- com.archimatetool.script/src/com/archimatetool/script/commands/SetCommand.java
+++ com.archimatetool.script/src/com/archimatetool/script/commands/SetCommand.java
@@ -8,44 +8,16 @@ package com.archimatetool.script.commands;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 
+import com.archimatetool.editor.model.commands.EObjectFeatureCommand;
+
 /**
  * SetCommand
  * 
  * @author Phillip Beauvoir
  */
-public class SetCommand extends ScriptCommand {
-    private EStructuralFeature feature;
-    private Object oldValue;
-    private Object newValue;
-    private EObject eObject;
+public class SetCommand extends ScriptCommandWrapper {
 
     public SetCommand(EObject eObject, EStructuralFeature feature, Object newValue) {
-        super("Script", eObject); //$NON-NLS-1$
-        this.feature = feature;
-        this.eObject = eObject;
-        oldValue = eObject.eGet(feature);
-        this.newValue = newValue;
-    }
-    
-    @Override
-    public void perform() {
-        eObject.eSet(feature, newValue);
-    }
-    
-    @Override
-    public void undo() {
-        eObject.eSet(feature, oldValue);
-    }
-    
-    @Override
-    public boolean canExecute() {
-        return (newValue != null) ? !newValue.equals(oldValue)
-                : (oldValue != null) ? !oldValue.equals(newValue)
-                : false;
-    }
-
-    @Override
-    public void dispose() {
-        eObject = null;
+        super(new EObjectFeatureCommand("Script", eObject, feature, newValue), eObject); //$NON-NLS-1$
     }
 }
