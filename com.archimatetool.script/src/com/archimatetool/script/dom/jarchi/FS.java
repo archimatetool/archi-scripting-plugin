83c905fb5182ea9dd1eefbcd744375cd0f881ce3
Dominic Winkler <d.winkler@flexarts.at>
2021-12-07 17:06:56 +0100

    + Add "$.fs.cleanFolder" function for cleaning a folder under the git workdirectory (e.g. "Views")

--- com.archimatetool.script/src/com/archimatetool/script/dom/jarchi/FS.java
+++ com.archimatetool.script/src/com/archimatetool/script/dom/jarchi/FS.java
@@ -9,7 +9,12 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Base64;
+import java.util.Comparator;
+
+import com.archimatetool.script.dom.model.CurrentModel;
 
 /**
  * File services
@@ -72,4 +77,54 @@ public class FS {
         File parent = file.getParentFile();
         return parent != null ? parent.mkdirs() : false;
     }
+    
+    /**
+     * Removes all files from folder
+     * @param path
+     * @throws IOException
+     */
+    public void cleanFolder(String path) throws IOException {
+    	CurrentModel model = new CurrentModel();
+    	File file = new File(model.getPath()); // /<base>/.git/temp.archimate
+    	File gitpath = file.getParentFile(); // /<base>/.git/
+    	File base = gitpath.getParentFile(); // /<base>/.git/
+    	File folder = new File(path);
+    	if (folder.isDirectory() && isSubDirectory(base, folder) && !isSubDirectory(gitpath, folder)) {
+	    	Path pathToBeDeleted = Path.of(path);
+	
+	    	Files.walk(pathToBeDeleted)
+		        .sorted(Comparator.reverseOrder())
+		        .map(Path::toFile)
+		        .forEach(File::delete);
+    	} else {
+    		System.err.println("Could not empty folder '" + path +"', not a directory or sub-directory of '"+ base.toString()+"'");
+    		//throw new IOException("Could not empty folder '" + path +"', not a directory or sub-directory of '"+ base.toString()+"'");
+    	}
+    	
+    }
+    
+    /**
+     * Checks, whether the child directory is a subdirectory of the base 
+     * directory.
+     *
+     * @param base the base directory.
+     * @param child the suspected child directory.
+     * @return true, if the child is a subdirectory of the base directory.
+     * @throws IOException if an IOError occured during the test.
+     */
+    private boolean isSubDirectory(File base, File child)
+        throws IOException {
+        base = base.getCanonicalFile();
+        child = child.getCanonicalFile();
+
+        File parentFile = child;
+        while (parentFile != null) {
+            if (base.equals(parentFile)) {
+                return true;
+            }
+            parentFile = parentFile.getParentFile();
+        }
+        return false;
+    }
+    
 }
