2842de312423d09c03ee4467e0ba148e80ca1c64
Phillipus <p.beauvoir@dadabeatnik.com>
2022-11-17 21:26:40 +0000

    [Tests] Remove test not valid since ArchiMate 3.2
    - Business Object can have an alternate figure

--- com.archimatetool.script.tests/src/com/archimatetool/script/dom/model/ArchimateDiagramModelObjectProxyTests.java
+++ com.archimatetool.script.tests/src/com/archimatetool/script/dom/model/ArchimateDiagramModelObjectProxyTests.java
@@ -192,17 +192,6 @@ public class ArchimateDiagramModelObjectProxyTests extends DiagramModelObjectPro
         assertEquals(1, actualTestProxy.attr(IModelConstants.FIGURE_TYPE));
     }
     
-    @Test
-    public void attr_FigureTypeCannotSet() {
-        testEObject = IArchimateFactory.eINSTANCE.createDiagramModelArchimateObject();
-        ((IDiagramModelArchimateObject)testEObject).setArchimateElement(IArchimateFactory.eINSTANCE.createBusinessObject());
-        actualTestProxy = (DiagramModelObjectProxy)EObjectProxy.get(testEObject);
-        
-        assertEquals(0, actualTestProxy.attr(IModelConstants.FIGURE_TYPE));
-        actualTestProxy.attr(IModelConstants.FIGURE_TYPE, 1);
-        assertEquals(0, actualTestProxy.attr(IModelConstants.FIGURE_TYPE));
-    }
-    
     @Override
     @Test
     public void attr_TextAlignment() {
