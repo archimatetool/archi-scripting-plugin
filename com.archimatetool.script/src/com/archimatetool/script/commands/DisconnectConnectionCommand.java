b46cf9a1868b62ea15b9e425b731c34a331a29d6
Phillipus <p.beauvoir@dadabeatnik.com>
2025-12-13 13:49:03 +0000

    Preserve connection order on undo
    - When a connection is disconnected and reconnected it is added at the end of the containing list. This causes a change in the order of the list and so the model XML will be different. This manifests in coArchi2.
    
    - Store old list positions and restore

--- com.archimatetool.script/src/com/archimatetool/script/commands/DisconnectConnectionCommand.java
+++ com.archimatetool.script/src/com/archimatetool/script/commands/DisconnectConnectionCommand.java
@@ -5,6 +5,8 @@
  */
 package com.archimatetool.script.commands;
 
+import org.eclipse.emf.common.util.EList;
+
 import com.archimatetool.model.IDiagramModelConnection;
 
 /**
@@ -14,26 +16,45 @@ import com.archimatetool.model.IDiagramModelConnection;
  */
 public class DisconnectConnectionCommand extends ScriptCommand {
     
-    private IDiagramModelConnection eObject;
+    private IDiagramModelConnection connection;
+    
+    // Store the positions of the connection in their lists
+    private int oldSourcePosition;
+    private int oldTargetPosition;
 
-    public DisconnectConnectionCommand(IDiagramModelConnection eObject) {
-        super("delete", eObject); //$NON-NLS-1$
-        this.eObject = eObject;
+    public DisconnectConnectionCommand(IDiagramModelConnection connection) {
+        super("delete", connection); //$NON-NLS-1$
+        this.connection = connection;
     }
 
     @Override
     public void undo() {
-        eObject.reconnect();
+        connection.reconnect();
+        
+        // restore these
+        EList<IDiagramModelConnection> sources = connection.getSource().getSourceConnections();
+        if(oldSourcePosition >= 0 && oldSourcePosition < sources.size() && sources.contains(connection)) {
+            sources.move(oldSourcePosition, connection);
+        }
+        
+        EList<IDiagramModelConnection> targets = connection.getTarget().getTargetConnections();
+        if(oldTargetPosition >= 0 && oldTargetPosition < targets.size() && targets.contains(connection)) {
+            targets.move(oldTargetPosition, connection);
+        }
     }
 
     @Override
     public void perform() {
-        eObject.disconnect();
+        // Store these here
+        oldSourcePosition = connection.getSource().getSourceConnections().indexOf(connection);
+        oldTargetPosition = connection.getTarget().getTargetConnections().indexOf(connection);
+
+        connection.disconnect();
     }
     
     @Override
     public void dispose() {
         super.dispose();
-        eObject = null;
+        connection = null;
     }
 }
--- com.archimatetool.script/src/com/archimatetool/script/commands/SetRelationshipOnDiagramModelConnectionCommand.java
+++ com.archimatetool.script/src/com/archimatetool/script/commands/SetRelationshipOnDiagramModelConnectionCommand.java
@@ -5,8 +5,11 @@
  */
 package com.archimatetool.script.commands;
 
+import org.eclipse.emf.common.util.EList;
+
 import com.archimatetool.model.IArchimateRelationship;
 import com.archimatetool.model.IDiagramModelArchimateConnection;
+import com.archimatetool.model.IDiagramModelConnection;
 
 /**
  * Set the Diagram Model Connections's IArchimateRelationship to the given relationship
@@ -18,6 +21,10 @@ public class SetRelationshipOnDiagramModelConnectionCommand extends ScriptComman
     private IArchimateRelationship newRelationship;
     private IDiagramModelArchimateConnection dmc;
     private IArchimateRelationship oldRelationship;
+    
+    // Store the positions of the connection in their lists
+    private int oldSourcePosition;
+    private int oldTargetPosition;
     
     /**
      * @param relationship The relationship to set on the dmc
@@ -33,6 +40,10 @@ public class SetRelationshipOnDiagramModelConnectionCommand extends ScriptComman
     
     @Override
     public void perform() {
+        // Store these here
+        oldSourcePosition = dmc.getSource().getSourceConnections().indexOf(dmc);
+        oldTargetPosition = dmc.getTarget().getTargetConnections().indexOf(dmc);
+
         // Safety to deregister listeners on the concept and update the UI
         dmc.disconnect();
         
@@ -41,6 +52,9 @@ public class SetRelationshipOnDiagramModelConnectionCommand extends ScriptComman
      
         // Reconnect and update UI
         dmc.reconnect();
+        
+        // Move these back
+        restoreConnectionPositions();
     }
     
     @Override
@@ -53,8 +67,24 @@ public class SetRelationshipOnDiagramModelConnectionCommand extends ScriptComman
         
         // Reconnect and update UI
         dmc.reconnect();
+        
+        // Move these back
+        restoreConnectionPositions();
     }
 
+    // Restore the connection positions in their lists
+    private void restoreConnectionPositions() {
+        EList<IDiagramModelConnection> sources = dmc.getSource().getSourceConnections();
+        if(oldSourcePosition >= 0 && oldSourcePosition < sources.size() && sources.contains(dmc)) {
+            sources.move(oldSourcePosition, dmc);
+        }
+        
+        EList<IDiagramModelConnection> targets = dmc.getTarget().getTargetConnections();
+        if(oldTargetPosition >= 0 && oldTargetPosition < targets.size() && targets.contains(dmc)) {
+            targets.move(oldTargetPosition, dmc);
+        }
+    }
+    
     @Override
     public void dispose() {
         super.dispose();
